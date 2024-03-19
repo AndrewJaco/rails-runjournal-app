22ce30325321a8c21309232957af7b4d6254620d
andrewjacobus <andrew@cogini.com>
2024-03-19 13:31:02 +0800

    jwt authorization with devise finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,7 @@
 class User < ApplicationRecord
-  before_create :set_jti
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   devise :database_authenticatable, :registerable,
          :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
-  def set_jti
-    self.jti = SecureRandom.uuid
-  end
-
-  def self.jwt_revoked?(payload, user)
-    user.jti != payload['jti']  
-  end
-
-  def self.on_jwt_dispatch(token, payload)
-    
 end
